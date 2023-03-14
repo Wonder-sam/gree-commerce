a2c2f62e79d0cfc1007993884e914254db5268db
wonder <samuel.nai@yahoo.com>
2023-03-14 16:44:59 +0000

    add dummy data

---
+++ src/dummydata.js
@@ -0,0 +1,125 @@
+import won8 from './assets/won (8).JPG'
+import won1 from './assets/won (1).JPG'
+import won2 from './assets/won (2).JPG'
+import won3 from './assets/won (3).JPG'
+import won4 from './assets/won (4).JPG'
+import won5 from './assets/won (5).JPG'
+import won6 from './assets/won (6).JPG'
+import won7 from './assets/won (7).JPG'
+import caro1 from './assets/caro (1).JPG'
+import caro2 from './assets/caro (2).JPG'
+import caro3 from './assets/caro (3).JPG'
+import caro4 from './assets/caro (4).JPG'
+import caro5 from './assets/caro (5).JPG'
+import caro6 from './assets/caro (6).JPG'
+import caro7 from './assets/caro (7).JPG'
+import caro8 from './assets/caro (8).JPG'
+import caro9 from './assets/caro (9).JPG'
+import caro10 from './assets/caro (10).JPG'
+
+export const categories = [
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won1
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won2
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won3
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won4
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won5
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won6
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won7
+    },
+    {
+        name: "Wonder",
+        description: "backpacks, sidebags, waist bags and many more",
+        img: won8
+    }
+]
+
+export const caro =[
+    {
+        img: caro1,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro2,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro3,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro4,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro5,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro6,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro7,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro8,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro9,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+    {
+        img: caro10,
+        title: "19BX Backpack",
+        info: "The all new awaited bag of the season. The 19BX Backpack. Grap yours now @ 12% discount !!!",
+        alt: "19BX backpack"
+    },
+
+]
