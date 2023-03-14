8ece6169862a9caee51a150c4212bf59de5b7111
wonder <samuel.nai@yahoo.com>
2023-03-14 12:45:53 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-2xl">
+          Hello, Ladies and Gentlemen 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="text-3xl">
+          Your Ecommerce app will be up soon
+        </p>
       </header>
     </div>
   );
